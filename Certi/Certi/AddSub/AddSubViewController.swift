ded90add685a528f192a404d03c0286d16322aa2
안소정 <80646857+ahneve@users.noreply.github.com>
2022-09-29 15:56:48 +0900

    [Feat] NavigationController SideMenu

---
+++ Certi/Certi/AddSub/AddSubViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddSubViewController.swift
+//  Certi
+//
+//  Created by 소정의 Mac on 2022/09/29.
+//
+
+import UIKit
+
+class AddSubViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Certi/Certi/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Certi
+//
+//  Created by 소정의 Mac on 2022/09/29.
+//
+
+import UIKit
+
+class HomeViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setMenuButton()
+        
+        //self.
+    }
+
+}
