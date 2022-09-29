6c877bd947e4fdcfa9761f2eb358515bfc29e53e
안소정 <80646857+ahneve@users.noreply.github.com>
2022-09-29 16:08:20 +0900

    [Fix] Re 프로젝트 생성

---
+++ Certi_012/Certi_012/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Certi_012
+//
+//  Created by 소정의 Mac on 2022/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
